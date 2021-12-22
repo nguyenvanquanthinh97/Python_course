dc238371eceef0a46a6e64433728d1179648248f
Thinh Nguyen <thinhnguyen@Thinhs-Mac-mini.local>
2021-12-22 23:45:10 +0700

    Add docs string in 20_function.py

--- 20_function.py
+++ 20_function.py
@@ -1,5 +1,7 @@
 def add (x, y):
+  """This function is used to handle sum of 2 parameters"""
   return x + y
 
 def reverse(text):
+  """This function is used to reverse array"""
   return text[::-1]
