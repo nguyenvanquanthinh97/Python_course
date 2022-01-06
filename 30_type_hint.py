7e2982a0fcf9d8765ffe8f19025ccb44b43ab963
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2022-01-06 13:38:21 +0700

    type-check

---
+++ 30_type_hint.py
@@ -0,0 +1,11 @@
+# age: int
+# name: str
+# height: float
+# is_human: false
+
+def police_check(age: int) -> bool:
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
